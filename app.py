7f6be3d1bfc9ba937e18d1384dfccd3a93cc22e6
unknown <brannongarrett77@gmail.com>
2023-12-23 21:18:09 +0100

    Updated multiple files and added code to fix issues

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -8,8 +9,11 @@ Session(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name=session.get("name"))
 
 @app.route('/login')
 def login():
+    if request.method == 'POST':
+        session["name"] = request.form.get("name")
+        return redirect('/')
     return render_template('login.html')
